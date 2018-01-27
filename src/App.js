c02cd77572ad2987f33ec322fc3d51ebe854cd1b
peatroot <garethpeat@gmail.com>
2018-01-27 12:07:05 +0000

    Use GenomeCoordinateChart

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 import Menu from './Menu';
 
 import 'semantic-ui-css/semantic.min.css';
-import PostgapTable from './tables/PostgapTable';
+// import PostgapTable from './tables/PostgapTable';
+import PostgapTableRedux from './tables/PostgapTableRedux';
 
 import { Card, Label, Statistic, Container, Header } from 'semantic-ui-react';
 import * as V from 'victory';
@@ -15,10 +16,26 @@ import EntitySelect from './EntitySelect';
 import EntitySet from './EntitySet';
 import Fullscreenable from './Fullscreenable';
 import FullscreenableGroup from './FullscreenableGroup';
+import BrowserChart from './charts/BrowserChart';
+
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './store/reducers/postgap.js';
+import GenomeCoordinateChart from './postgap/chart/GenomeCoordinateChart';
+// import PostgapTable from './tables/PostgapTable';
+
+const loggerMiddleware = createLogger();
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
+
+
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -29,10 +46,81 @@ class App extends Component {
         </p>
 
 
+        <Card fluid>
+            <Card.Content align={'left'}>
+              <EntitySelect />
+            </Card.Content>
+          </Card>
 
-        <Container blue>
+          <Card fluid>
+            <Card.Content>
+              <GenomeCoordinateChart />
+            </Card.Content>
+          </Card>
 
-        <FullscreenableGroup>
+          {/* <Card fluid>
+            <Card.Content>
+              <BrowserChart />
+            </Card.Content>
+          </Card> */}
+
+          {/* <PostgapTableRedux/> */}
+
+
+          {/* <Card>
+            <Card.Content>
+              <V.VictoryChart
+                    containerComponent={
+                        <V.VictoryBrushContainer
+                        brushDimension="x"
+                        brushDomain={{x: [0, 1]}}
+                        />
+                    }
+                    >
+                    <V.VictoryBar 
+                        data={[
+                            {x: 0, y: 5},
+                            {x: 0.1, y: 6},
+                            {x: 0.3, y: 4.5},
+                            {x: 0.6, y: 7},
+                            {x: 0.9, y: 2},
+                        ]}
+                    />
+                </V.VictoryChart>
+            </Card.Content>
+          </Card>
+
+          <Card>
+            <Card.Content>
+              <V.VictoryChart domainPadding={20} theme={V.VictoryTheme.material}>
+                <V.VictoryAxis
+                  label={'2017'}
+                  tickValues={[1, 2, 3, 4]}
+                  tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+                />
+                <V.VictoryAxis
+                  dependentAxis
+                  tickFormat={(x) => (`$${x}k`)}
+                />
+                <V.VictoryBar />
+              </V.VictoryChart>
+            </Card.Content>
+          </Card>
+
+
+
+
+
+          <Card>
+            <Card.Content>
+              <PostgapChart />
+            </Card.Content>
+          </Card> */}
+
+
+        <Container>
+
+        {/* <FullscreenableGroup>
           <Fullscreenable id='first'>
             <Statistic.Group size='small' align='center'>
               <Statistic label='Allele' value='A/G'></Statistic>
@@ -41,7 +129,7 @@ class App extends Component {
           
           </Fullscreenable>
           <Fullscreenable id='second'>B</Fullscreenable>
-        </FullscreenableGroup>
+        </FullscreenableGroup> */}
 
 {/* 
         <Card.Group itemsPerRow={2}>
@@ -141,6 +229,7 @@ class App extends Component {
 
         </Container>
       </div>
+      </Provider>
     );
   }
 }
